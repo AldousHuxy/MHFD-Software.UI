3d4a62c8d06f5a3f71a2d259cea834bfeee6abef
jhoward <jhoward@mhfd.org>
2026-01-22 15:32:24 -0700

    env

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
-export const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:5000/api";
+export const serverUrl = import.meta.env.VITE_SERVER_URL || "https://mhfd-software-api.onrender.com/api";
 export const username = import.meta.env.VITE_USERNAME;
 export const password = import.meta.env.VITE_PASSWORD;
